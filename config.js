d4124cfff0d2860a7da301dab7431832b5d0171e
ktupad <ktupad@gmail.com>
2021-06-02 21:46:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-conf.dataApp.host='http://localhost/new/';
+// conf.dataApp.host='http://localhost/new/';
 conf.dataApp.host='https://ktupad.github.io/';
 conf.dataApp.model='https://sismadi.co.id/api/';
 conf.isSignup='hide';
