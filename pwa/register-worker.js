f4b8cf87d0e2138a476065c33f176c5bf24b0c11
ktupad <ktupad@gmail.com>
2021-06-02 21:36:07 +0700

    add new version

--- pwa/register-worker.js
+++
@@ -1,8 +0,0 @@
-/**
- * Register the service worker
- */
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('service-worker.js')
-		.then(function () { console.log('Service Worker Registered'); });
-}
