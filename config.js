206de909b4f72adf97fdc93aafc22f12d3990bc1
ktupad <30571927+ktupad@users.noreply.github.com>
2021-06-02 22:15:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,25 +4,5 @@ conf.dataApp.model='https://sismadi.co.id/api/';
 conf.isSignup='hide';
 conf.isDb=1;
 conf.akses=['c','r','u','d'];
-// conf.getappdata();
+conf.getappdata();
 // conf.open('atas');
-
-getappdata=function(){
-  var data=window.localStorage.getItem('appData');
-  if(!data){
-    datas={token:'1258',mod:'app',cond:''};
-    datas.host='https://ktupad.github.io/';
-     conf.loadModel(datas,function(res){
-     json=JSON.parse(res);
-     debug(json);
-     window.localStorage.setItem('appData', JSON.stringify(json));
-     conf.getapp(res);
-      });
-  }
-  else{
-  var data=window.localStorage.getItem('appData');
-  conf.getapp(data);
-  }
-};
-
-getappdata();
